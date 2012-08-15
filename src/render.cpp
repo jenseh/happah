f26c27f87083c041460f8c4982d0a7fbee6b5135
Matthias Sauer <msauer@ira.uka.de>
2012-08-15 13:55:05 +0200

    Added OpenGl Window

---
+++ src/render.cpp
@@ -0,0 +1,57 @@
+/*
+ * render.cpp
+ *
+ *  Created on: 15.08.2012
+ *      Author: msauer
+ */
+
+#include <QtGui>
+#include <QtOpenGL>
+#include "render.h"
+
+RenderClass::RenderClass(QWidget *parent) : QGLWidget(parent)
+{
+	setFormat(QGLFormat(QGL::DoubleBuffer| QGL::DepthBuffer) );
+
+}
+
+
+
+
+void RenderClass::initializeGL()
+{
+	glShadeModel(GL_SMOOTH);
+	glClearColor(0.0f,0.0f,0.0f,1.0f);
+	glClearDepth(1.0f);
+	glEnable(GL_DEPTH_TEST);
+
+}
+
+void RenderClass::resizeGL(int width, int height)
+{
+	glViewport(0,0,width,height);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,20.0f);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void RenderClass::paintGL()
+{
+	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+	glBegin(GL_TRIANGLES);
+
+	glVertex3f(-0.5f,-0.5f,0.0f);
+	glColor3f(255.0f,0.0f,0.0f);
+
+	glVertex3f(0.0f,0.5f,0.0f);
+	glColor3f(0.0f,255.0f,0.0f);
+
+	glVertex3f(0.5f,-0.5f,0.0f);
+	glColor3f(0.0f,0.0f,255.0f);
+	glEnd();
+}
+
+
+
---
+++ src/render.h
@@ -0,0 +1,33 @@
+/*
+ * render.h
+ *
+ *  Created on: 15.08.2012
+ *      Author: msauer
+ */
+
+#ifndef RENDER_H_
+#define RENDER_H_
+
+// TODO : Create a Base Render Class
+
+#include <QGLWidget>
+#include <QWidget>
+
+class RenderClass : public QGLWidget
+{
+	Q_OBJECT
+
+// TODO: Set private functions
+
+
+public:
+	RenderClass(QWidget *parent=0);
+
+protected:
+	void initializeGL();
+	void resizeGL(int width, int height);
+	void paintGL();
+
+};
+
+#endif /* RENDER_H_ */

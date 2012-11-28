f8612a027f6e4b7a71c531eeb70ba265d03969d8
EmjayS <sauer.m.j@googlemail.com>
2012-11-28 19:21:24 +0100

    Added a GeometryObject Core Class

---
+++ src/main/core/GeometryObject.cpp
@@ -0,0 +1,18 @@
+/*
+ * GeomtryObject.cpp
+ *
+ *  Created on: Nov 28, 2012
+ *      Author: matthias
+ */
+
+#include "GeometryObject.h"
+
+GeomtryObject::GeomtryObject() {
+	// TODO Auto-generated constructor stub
+
+}
+
+GeomtryObject::~GeomtryObject() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/main/core/GeometryObject.h
@@ -0,0 +1,17 @@
+/*
+ * GeomtryObject.h
+ *
+ *  Created on: Nov 28, 2012
+ *      Author: matthias
+ */
+
+#ifndef GEOMTRYOBJECT_H_
+#define GEOMTRYOBJECT_H_
+
+class GeomtryObject {
+public:
+	GeomtryObject();
+	virtual ~GeomtryObject();
+};
+
+#endif /* GEOMTRYOBJECT_H_ */

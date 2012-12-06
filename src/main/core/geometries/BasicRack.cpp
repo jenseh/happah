fa65232832f49201b63cedea613d89f34d8a7f49
jlabeit <j.labeit@web.de>
2012-12-06 13:26:27 +0100

    Started to implement the 3D representation of a basic rack.

---
+++ src/main/core/geometries/BasicRack.cpp
@@ -0,0 +1,112 @@
+#include "BasicRack.h"
+
+// Constructor for a general gear. Gears are always centered on 0,0,0 with the z axis being the gear axis.
+BasicRack::BasicRack(float length, float depth, float height, int toothCount) :
+        GeometryObject() {
+    length_ = length;
+    depth_ = depth;
+    height_ = height;
+    toothCount_ = toothCount;
+    // Modul = Teilung / PI
+    module_ = (length / (float)toothCount) / M_PI;
+}
+
+BasicRack::~BasicRack() {
+}
+
+// Create a profile of height values for one partition (german: Teilung)
+// x-values must be between 0 and p (partition, german: Teilung)
+// y-values must be between -p and p (height)
+void BasicRack::createHeightProfilePartition() {
+    heightProfilePartition_ = std::vector<glm::vec2>();
+
+    standardProfile = new StandardProfile(module_, 10 / 180.0 * M_PI, 0, 0);
+    standardProfile->getProfilePartition(heightProfilePartition_,
+            SEGMENT_COUNT);
+    //createApproximatedPartition();
+}
+
+// Create a profile of height values
+// x-values must be between 0 and 1 (position)
+// y-values must be between -1 and 1 (height)
+void BasicRack::createHeightProfile() {
+    createHeightProfilePartition();
+    heightProfile_ = std::vector<glm::vec2>();
+
+    for (int i = 0; i < toothCount_; i++) {
+        for (unsigned int j = 0; j < heightProfilePartition_.size(); j++) {
+            float position = heightProfilePartition_[j].x + i * module_ * M_PI;
+            float height = heightProfilePartition_[j].y;
+            heightProfile_.push_back(glm::vec2(position, height));
+            //cout << position << ": " << height << std::endl;
+        }
+    }
+    // Go down and back to the start
+    glm::vec2 tempVec = heightProfile_[heightProfile_.size() - 1];
+    tempVec.y -= height_; // Mach unten
+    heightProfile_.push_back(tempVec);
+    tempVec = heightProfile_[0]; // nach links
+    tempVec.y -= height_; // und unten
+    heightProfile_.push_back(tempVec);
+    tempVec.y += height_; // und wieder hoch
+    heightProfile_.push_back(tempVec);
+
+    //heightProfile_.push_back(heightProfile_[0]);
+}
+
+// This creates the quads for a gear. The gear axis is the model's z-axis.
+void BasicRack::createVertexData() {
+    float dz = depth_ / Z_DETAIL_LEVEL;
+
+    // Create the height profile given the current gear settings
+    createHeightProfile();
+    for (int i = 0; i < Z_DETAIL_LEVEL; i++) {
+        float z = i * dz;
+        glm::vec4 a, b, c, d, norm;
+        a.z = z;
+        a.w = 1.0f;
+
+        b.z = z;
+        b.w = 1.0f;
+
+        c.z = z + dz;
+        c.w = 1.0f;
+
+        d.z = z + dz;
+        d.w = 1.0f;
+        for (unsigned int segmentNum = 0; segmentNum < heightProfile_.size()-1;
+                segmentNum++) {
+
+            a.x = heightProfile_[segmentNum + 1].x;
+            a.y = heightProfile_[segmentNum + 1].y;
+
+            b.x = heightProfile_[segmentNum].x;
+            b.y = heightProfile_[segmentNum].y;
+
+            c.x = heightProfile_[segmentNum].x;
+            c.y = heightProfile_[segmentNum].y;
+
+            d.x = heightProfile_[segmentNum + 1].x;
+            d.y = heightProfile_[segmentNum + 1].y;
+
+            norm = glm::vec4(
+                    glm::normalize(
+                            glm::cross(glm::vec3(0.0f, 0.0f, c.z - b.z),
+                                    glm::vec3(a.x - b.x, a.y - b.y, 0.0f))),
+                    1.0f);
+
+            dataPushback(a);
+            //dataPushback(normNext);
+            dataPushback(norm);
+            dataPushback(b);
+            dataPushback(norm);
+            dataPushback(c);
+            dataPushback(norm);
+            dataPushback(d);
+            //dataPushback(normNext);
+            dataPushback(norm);
+        }
+    }
+
+
+}
---
+++ src/main/core/geometries/BasicRack.h
@@ -0,0 +1,37 @@
+#ifndef BASICRACK_H
+#define BASICRACK_H
+
+#include <glm/glm.hpp>
+#include <iostream>
+#include <math.h>
+#include "GeometryObject.h"
+#include "BasicRack.h"
+#include "StandardProfile.h"
+
+using namespace std;
+
+
+class BasicRack: public GeometryObject {
+public:
+    BasicRack(float lenght = 2.0f, float depth = 0.6f, float height = 0.7f, int toothCount = 5);
+
+    virtual ~BasicRack();
+    void createVertexData();
+private:
+    float height_; // Länge der Stange
+    float depth_; // Tiefe auf z Achse
+    float length_; // Länge
+    int toothCount_; // Anzahl der Zähne
+    float module_; // Modul des Standartprofils
+    std::vector<glm::vec2> heightProfilePartition_;
+    std::vector<glm::vec2> heightProfile_;
+    StandardProfile* standardProfile;
+
+    const static int SEGMENT_COUNT = 40; //maximum: 4500
+    const static int Z_DETAIL_LEVEL = 4;
+
+    void createHeightProfilePartition();
+    void createHeightProfile();
+};
+
+#endif // BASICRACK_H

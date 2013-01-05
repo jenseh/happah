2d2af9aacb2cc398744ce05f16daf89a88ac9955
jlabeit <j.labeit@web.de>
2013-01-05 16:37:11 +0100

    Continuing simulation implementation.

--- src/main/core/simulations/DiscGearGrind.cpp
+++ src/main/core/simulations/DiscGearGrind.cpp
@@ -18,8 +18,15 @@ void DiscGearGrind::calculateGrindingDepth(){
     vector<double> distances(gearMesh.size());
     for( size_t i = 0; i < gearMesh.size(); i++){
         IntersectInfo info;
-        kdTree.intersect(*gearMesh[i], info);
-        distances[i] = glm::distance(gearMesh[i]->origin , info.hit_point);
+        distances[i] = 100;
+        Ray inverseRay = *gearMesh[i];
+        inverseRay.direction = gearMesh[i]->inverse_direction;
+        inverseRay.inverse_direction = gearMesh[i]->direction;
+        if( kdTree.intersect(inverseRay, info)){
+            distances[i] = -glm::distance(inverseRay.origin , info.hit_point);
+        }else if( kdTree.intersect(*gearMesh[i], info) ){
+            distances[i] = glm::distance(gearMesh[i]->origin , info.hit_point);
+        }
     }
     return;
 }

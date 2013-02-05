e334d8f2dd0b12f4ccd26a2f6091b6e949e3c78a
katja <ich-bin-sowieso@abwesend.de>
2013-02-05 15:18:24 +0100

    Restored LinearKinematic classes as they appear to have spirited away somehow

---
+++ src/main/core/simulations/LinearKinematic.cpp
@@ -0,0 +1,13 @@
+#include "LinearKinematic.h"
+
+LinearKinematic::LinearKinematic(int iterations, glm::vec3 start, glm::vec3 end):Kinematic(iterations)
+{
+    m_CurrentMatrix = glm::translate(glm::mat4(1.0f), start);
+    m_Delta = end - start;
+    m_Delta /= (float)iterations;
+}
+
+void LinearKinematic::calcNextMatrix(){
+    m_CurrentMatrix = glm::translate(m_CurrentMatrix, m_Delta);
+    Kinematic::calcNextMatrix();
+}

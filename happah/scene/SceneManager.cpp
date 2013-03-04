fd415039280905e2bcc45b568ed5296d8d8b389a
Pawel Herman <pawel.herman@kit.edu>
2013-03-04 09:51:39 +0100

    cleaned up scene manager

--- happah/scene/SceneManager.cpp
+++ happah/scene/SceneManager.cpp
@@ -47,71 +47,11 @@ void SceneManager::remove(InvoluteGear_ptr involuteGear, TriangleMesh_ptr triang
 	notifyListeners();
 }
 
-void SceneManager::visit(InvoluteGearNode& involuteGearNode) {}
-
-void SceneManager::visit(TriangleMeshNode& triangleMeshNode) {
-	m_vectorDrawables->push_back(triangleMeshNode.getGeometry().get());
-}
-
-void SceneManager::visit(TriangleMeshRenderStateNode& triangleMeshRenderStateNode) {}
-
-//first idDrawable gets id 0, m_idCounter is has number of produced idDrawables
-uint SceneManager::addDrawable(Drawable *drawable) {
-    IdDrawable idDrawable = {m_iDCounter, drawable};
-    m_drawables.push_back(idDrawable);
-    m_iDCounter += 1;
-    notifyListeners();
-    return idDrawable.id;
-}
-
-void SceneManager::removeDrawable(uint id) {
-    std::list<IdDrawable>::iterator candidate = m_drawables.end();
-    for(std::list<IdDrawable>::iterator it = m_drawables.begin(), end = m_drawables.end(); it != end; ++it) {
-        if (it->id == id) {
-            candidate = it;
-        }
-    }
-    if(candidate != m_drawables.end()){
-        m_drawables.erase(candidate);
-        notifyListeners();
-    }
-}
-
-vector<Drawable*>* SceneManager::getDrawables() {
-	m_vectorDrawables = new vector<Drawable*>();
-
-	accept(*this);
-
-	return m_vectorDrawables;
-}
-
-
-void SceneManager::addNonDrawable(NonDrawable *nonDrawable){
-  IdNonDrawable idNonDrawable = {m_iDNCounter, nonDrawable};
-  m_nonDrawables.push_back(idNonDrawable);
-  m_iDNCounter += 1;
-}
-
-std::vector<NonDrawable*>* SceneManager::getNonDrawables(){
-  std::vector<NonDrawable*> *nonDrawables = new std::vector<NonDrawable*>;
-
-  for(std::list<IdNonDrawable>::iterator it = m_nonDrawables.begin(); it != m_nonDrawables.end(); ++it) {
-      nonDrawables->push_back(it->nonDrawable);
-  }
-  return nonDrawables;
-}
-
-void SceneManager::drawablesChanged() {
-    notifyListeners();
-  }
-
 void SceneManager::registerListener(SceneListener* sceneListener) {
-    m_listeners.push_back(sceneListener);
-  }
+	m_listeners.push_back(sceneListener);
+}
 
 void SceneManager::notifyListeners() {
-  for(std::list<SceneListener*>::iterator it = m_listeners.begin(), end = m_listeners.end();
-    it != end; ++it) {
-    (*it)->sceneChanged();
-  }
+	for(std::list<SceneListener*>::iterator it = m_listeners.begin(), end = m_listeners.end(); it != end; ++it)
+		(*it)->sceneChanged();
 }

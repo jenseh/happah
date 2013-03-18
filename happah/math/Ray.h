aae40203e4ff691c619d155e248c5c4aaabfc237
jlabeit <jlabeit@web.de>
2013-03-18 11:39:57 +0100

    Changed ray to class with private members.

--- happah/math/Ray.h
+++ happah/math/Ray.h
@@ -4,21 +4,25 @@
 #include "happah/kdtree/BBox.h"
 #include "happah/math/Triangle.h"
 
-struct Ray
+class Ray
 {
-    hpvec3 origin;				// Origin point of the ray.
-    hpvec3 direction;			// Normalized vector pointing in the direction of the ray.
+private:
+    hpvec3 m_origin;				// Origin point of the ray.
+    hpvec3 m_direction;			// Normalized vector pointing in the direction of the ray.
+
+
+    bool checkLineBox( hpvec3 boxPointMin, hpvec3 boxPointMax, hpvec3 linePoint1, hpvec3 linePoint2, hpvec3 &hitPoint);
+    bool getIntersection( float fDst1, float fDst2, hpvec3 P1, hpvec3 P2, hpvec3 &Hit);
+    bool inBox( hpvec3 hitPoint, hpvec3 boxPointMin, hpvec3 boxPointMax, const int axis);
+
+public:
 
     Ray();
     Ray(hpvec3 orig, hpvec3 dir);
     hpreal intersectDistance(Triangle& triangle);
     bool intersects(BBox& box, hpreal length);
     void transform(hpmat4x4& matrix);
+    void moveOrigin(hpreal delta);
 
-
-private:
-    bool checkLineBox( hpvec3 boxPointMin, hpvec3 boxPointMax, hpvec3 linePoint1, hpvec3 linePoint2, hpvec3 &hitPoint);
-    bool getIntersection( float fDst1, float fDst2, hpvec3 P1, hpvec3 P2, hpvec3 &Hit);
-    bool inBox( hpvec3 hitPoint, hpvec3 boxPointMin, hpvec3 boxPointMax, const int axis);
 
 };

c0c39b71976c0c662a829e12b43aa213bfd078d0
katja <ich-bin-sowieso@abwesend.de>
2013-04-08 15:12:50 +0200

    Changed vertexData to verticesAndNormals in Gear classes and changed some implementation details of CylindricalGear

--- happah/geometries/gears/CylindricalGear.cpp
+++ happah/geometries/gears/CylindricalGear.cpp
@@ -29,31 +29,33 @@ void CylindricalGear::getTraverseProfile(hpreal z, vector<hpvec2>& gearProfile) 
 }
 
 TriangleMesh* CylindricalGear::toTriangleMesh(hpuint toothSampleSize, hpuint zSampleSize) {
-	// Create vector for the result
-	vector<hpvec3>* vertexData = new vector<hpvec3>; //TODO: initialize vetexData => how many do I need?
-	vector<hpuint>* indices = new vector<hpuint>; //TODO initialize indices => how many do I need?
 	vector<hpvec2> profile(toothSampleSize * getNumberOfTeeth());
-	hpvec3 wildcardNormal = hpvec3(0.0f, 0.0f, 0.0f);
+	// Create vectors for the triangle mesh
+	vector<hpvec3>* verticesAndNormals = new vector<hpvec3>();
+	vector<hpuint>* indices = new vector<hpuint>();
+	verticesAndNormals->reserve((zSampleSize + 1) * profile.size() * 2); // two times for vertices and normals
+	indices->reserve(zSampleSize * profile.size() * 6);
 
+	hpvec3 wildcardNormal = hpvec3(0.0f, 0.0f, 0.0f);
 	hpreal dz = getFaceWidth() / zSampleSize;
 
 	for(hpuint i = 0; i <= zSampleSize; ++i) {
 		getTraverseProfile(i * dz, profile);
 		hpuint pointIndex = 0;
 		for(vector<hpvec2>::iterator j = profile.begin(), end = profile.end(); j != end; ++j) {
-			vertexData->push_back(hpvec3(j->x, j->y, i * dz));
-			vertexData->push_back(wildcardNormal);
+			verticesAndNormals->push_back(hpvec3(j->x, j->y, i * dz));
+			verticesAndNormals->push_back(wildcardNormal);
 
 			if(i != zSampleSize) {
 				hpuint nextPointIndex = ((*j) == profile.back()) ? 0 : (pointIndex + 1);
 
-				indices->push_back(i * profile.size() + nextPointIndex);		//0
-				indices->push_back(i * profile.size() + pointIndex);			//1
-				indices->push_back((i + 1) * profile.size() + pointIndex);	//2
+				indices->push_back(i * profile.size() + nextPointIndex);  //0
+				indices->push_back(i * profile.size() + pointIndex);  //1
+				indices->push_back((i + 1) * profile.size() + pointIndex);  //2
 				
-				indices->push_back(i * profile.size() + nextPointIndex);		//0
-				indices->push_back((i + 1) * profile.size() + pointIndex);	//2
-				indices->push_back((i + 1) * profile.size() + nextPointIndex);//3
+				indices->push_back(i * profile.size() + nextPointIndex);  //0
+				indices->push_back((i + 1) * profile.size() + pointIndex);  //2
+				indices->push_back((i + 1) * profile.size() + nextPointIndex);  //3
 			}
 			++pointIndex;
 		}
@@ -64,7 +66,7 @@ TriangleMesh* CylindricalGear::toTriangleMesh(hpuint toothSampleSize, hpuint zSa
 	hpuint indicesInRow = indices->size() / zSampleSize;
 	hpuint trianglePairsInRow  = indicesInRow / 6;
 
-	//array steps is necessary to walk in the vertexData array to the right places
+	//array steps is necessary to walk in the verticesAndNormals array to the right places
 	int steps[] = {0, 3, 4, -(indicesInRow - 3), -2, -3};
 
 	// go one step further in width direction to reach all points
@@ -76,7 +78,7 @@ TriangleMesh* CylindricalGear::toTriangleMesh(hpuint toothSampleSize, hpuint zSa
 			hpvec3 normal = hpvec3(0.0f);
 			int n = i * indicesInRow + j * 6;
 			for(hpuint k = 0; k < 6; ++k) {
-				int da, db; //distances in vertexData array to other two triangle points
+				int da, db; //distances in verticesAndNormals array to other two triangle points
 				if(k < 2) {
 					da = 2; db = 1;
 				} else if (k < 4) {
@@ -91,8 +93,8 @@ TriangleMesh* CylindricalGear::toTriangleMesh(hpuint toothSampleSize, hpuint zSa
 					n -= indicesInRow;
 				//not every point has 6 surrounding triangles. Use only the ones available:
 				if(n >= 0 && n < indices->size()) {
-					hpvec3 a = vertexData->at(2 * indices->at(n + da)) - vertexData->at(2 * indices->at(n));
-					hpvec3 b = vertexData->at(2 * indices->at(n + db)) - vertexData->at(2 * indices->at(n));
+					hpvec3 a = verticesAndNormals->at(2 * indices->at(n + da)) - verticesAndNormals->at(2 * indices->at(n));
+					hpvec3 b = verticesAndNormals->at(2 * indices->at(n + db)) - verticesAndNormals->at(2 * indices->at(n));
 					normal = normal + (hpvec3(glm::cross(a, b)));
 				}
 			}
@@ -105,10 +107,10 @@ TriangleMesh* CylindricalGear::toTriangleMesh(hpuint toothSampleSize, hpuint zSa
 					n -= indicesInRow;
 				//not every point has 6 surrounding triangles. Use only the ones available:
 				if(n >= 0 && n < indices->size())
-					vertexData->at(2 * indices->at(n) + 1) = -glm::normalize(normal); //insert the normal in the cell after the vertex
+					verticesAndNormals->at(2 * indices->at(n) + 1) = -glm::normalize(normal); //insert the normal in the cell after the vertex
 			}
 		}
 	}
 
-	return new TriangleMesh(vertexData, indices);
+	return new TriangleMesh(verticesAndNormals, indices);
 }

d75e22ab8012319acf1dff5ef391fee28be80b61
Julian Labeit <julian@julian-D2778-A1.(none)>
2013-04-12 12:40:16 +0200

    Added documentation to discgeargrind

--- happah/simulations/DiscGearGrind.cpp
+++ happah/simulations/DiscGearGrind.cpp
@@ -27,16 +27,11 @@ DiscGearGrind::~DiscGearGrind() {
 
 void DiscGearGrind::calculateGrindingDepth(double time){
     hpmat4x4 matrix = m_gearMovement.getMatrix(time);
-    vector<Triangle>* triangles = m_discMesh->toTriangles();
     for( size_t i = 0; i < m_gearRays->size(); i++){
         // Transform Ray
         Ray ray = m_gearRays->at(i);
-        //ray.print();
         ray.transform(matrix);
-        //ray.print();
         ray.moveOrigin(-m_maxDistance);
-        //ray.print();
-        //std::cout<<std::endl;
 
         m_distances[i] = 2 * m_maxDistance;
         m_distances[i] = (m_kdTree->intersectFirst(ray, m_maxDistance * 2) - m_maxDistance) / m_maxDistance;
@@ -48,26 +43,11 @@ DiscGearGrindResult DiscGearGrind::calculateSimulationResult(double time){
     calculateGrindingDepth(time);
     // Fill color
     for( size_t i = 0; i < m_gearColor->size(); i++){
-
-    	if( m_distances[i] < 0 ){
-    		//std::cout<<i<<std::endl;
-    		m_gearColor->at(i) = hpcolor(1.0,0.0,0.0,1.0);
-    	}else if( m_distances[i] < 0.9999){
-    		//std::cout<<i<<std::endl;
-    		m_gearColor->at(i) = hpcolor(0.0,1.0,0.0,1.0);
-    	}else{
-    		m_gearColor->at(i) = hpcolor(1.0,1.0,1.0,1.0);
-    	}
-    	/*
     	if( m_distances[i] >= 0 ){
-    		//m_gearColor->at(i) = hpcolor(0.0, 1.0, 0.0, 1.0);
     		m_gearColor->at(i) = hpcolor( 0.0, m_distances[i], 1.0, 1.0);
     	}else{
-    		m_gearColor->at(i) = hpcolor(1.0, 0.0, 0.0, 1.0);
-    		//m_gearColor->at(i) = hpcolor(1.0, 1.0 + m_distances[i], 1.0 + m_distances[i], 1.0);
+            m_gearColor->at(i) = hpcolor(1.0, 1.0 + m_distances[i], 1.0 + m_distances[i], 1.0);
     	}
-    	*/
-
     }
     return DiscGearGrindResult(m_gear, m_gearColor, m_gearMesh,  RigidAffineTransformation(),  m_disc, m_discMesh, m_gearMovement.getRigidAffineTransformation(time).inverse());
 }
@@ -81,7 +61,8 @@ DiscGearGrindResult DiscGearGrind::getSimulationResult(double time){
 }
 
 void DiscGearGrind::runSimulation(){
-	hpreal deltaT = 1.0 / (hpreal)STEP_COUNT;
+    // "-1.0"because 0.0 and 1.0 have to be calculated.
+    hpreal deltaT = 1.0 / ((hpreal)STEP_COUNT-1.0);
 	m_precalcResults.clear();
 	for( hpreal t = 0.0; t <= 1.0; t += deltaT ) {
 		m_precalcResults.insert(pair<hpreal, DiscGearGrindResult>(t, calculateSimulationResult(t)));
--- happah/simulations/DiscGearGrind.h
+++ happah/simulations/DiscGearGrind.h
@@ -10,23 +10,58 @@
 #include "happah/geometries/SurfaceOfRevolution.h"
 #include "happah/simulations/Kinematic.h"
 
-
+/**
+ * @brief The DiscGearGrindResult class bundles all the information needed to display a disc gear grind simulation to a given time.
+ */
 class DiscGearGrindResult : public SimulationResult {
 public:
+    /**
+     * @brief m_gear The gear of the simulation.
+     */
 	SimpleGear_ptr m_gear;
+    /**
+     * @brief m_gearColor Vector with the color data of the gear.
+     */
 	std::vector<hpcolor>* m_gearColor;
+    /**
+     * @brief m_gearMesh  The triangle mesh representing the gear.
+     */
 	TriangleMesh_ptr m_gearMesh;
+    /**
+     * @brief m_gearTransformation The position/transformation of the gear.
+     */
 	RigidAffineTransformation m_gearTransformation;
+    /**
+     * @brief m_tool Surface of revolution representing the tool used to grind the gear.
+     */
 	SurfaceOfRevolution_ptr m_tool;
+    /**
+     * @brief m_toolColor The unified color of the tool.
+     */
     hpcolor m_toolColor;
+    /**
+     * @brief m_toolMesh The triangle mesh representing the tool.
+     */
 	TriangleMesh_ptr m_toolMesh;
+    /**
+     * @brief m_toolTransformation The position/transformation of the tool.
+     */
 	RigidAffineTransformation m_toolTransformation;
 
-
+    /**
+     * @brief DiscGearGrindResult sets all the member variabeles.
+     * @param gear The gear of the simulation.
+     * @param gearColor Vector with the color data of the gear.
+     * @param gearMesh The triangle mesh representing the gear.
+     * @param gearTransformation The position/transformation of the gear.
+     * @param tool Surface of revolution representing the tool used to grind the gear.
+     * @param toolMesh The triangle mesh representing the tool.
+     * @param toolTransformation The position/transformation of the tool.
+     */
 	DiscGearGrindResult(SimpleGear_ptr gear, vector<hpcolor>* gearColor, TriangleMesh_ptr gearMesh, RigidAffineTransformation gearTransformation,
 						SurfaceOfRevolution_ptr tool, TriangleMesh_ptr toolMesh, RigidAffineTransformation toolTransformation):
 		m_gear(gear),
-		m_gearColor(new std::vector<hpcolor>(*gearColor) ), // Make copy !
+        m_gearColor(new std::vector<hpcolor>(*gearColor) ),
 		m_gearMesh(gearMesh),
 		m_gearTransformation(gearTransformation),
 		m_tool(tool),
@@ -41,32 +76,59 @@ class DiscGearGrind : public Simulation
 {
 
 public:
+    /**
+     * @brief DiscGearGrind constructor doing some minor precalculations for the simulation.
+     * @param disc The disc which is used to grind the gear in the simulation.
+     * @param discMesh The triangle mesh representing the disc.
+     * @param gear The gear which is grinded by the gear in the simulation.
+     * @param gearMesh The triangle mesh representing the gear.
+     */
     DiscGearGrind(SurfaceOfRevolution_ptr disc, TriangleMesh_ptr discMesh, SimpleGear_ptr gear, TriangleMesh_ptr gearMesh);
+    /**
+     * @brief ~DiscGearGrind Destructor deleting memory which was allocated for the calculation of the simulation.
+     */
     ~DiscGearGrind();
 
+    /**
+     * @brief getSimulationResult returns the simulation result at a certain time.
+     *        There are only descrete number of time step precalculated, so the latest result before time is returned.
+     * @param time The time of the simulation result ( time has to be in the interval [0,1] ).
+     * @return The simulation result with all the information needed to display the simulation at the given time.
+     */
     DiscGearGrindResult getSimulationResult(double time);
+    /**
+     * @brief runSimulation precalculates the STEP_COUNT simulation steps and stores them in private member m_precalcResults.
+     */
     void runSimulation();
 
 private:
+    /**
+     * @brief STEP_COUNT Number of time steps calculated for the simulation ( eg. if STEP_COUNT = 3 then steps t = 0, t = 0.5, t = 1 are calculated ).
+     */
 	static const int STEP_COUNT = 5;
-	std::map< hpreal,DiscGearGrindResult> m_precalcResults;
 
     SurfaceOfRevolution_ptr m_disc;
-    SimpleGear_ptr m_gear;
-
     TriangleMesh_ptr m_discMesh;
+    std::vector<double> m_distances;
+    SimpleGear_ptr m_gear;
     vector<hpcolor>* m_gearColor;
     TriangleMesh_ptr m_gearMesh;
+    Kinematic m_gearMovement;
     std::vector<Ray>* m_gearRays;
     KDTree* m_kdTree;
-
     hpreal m_maxDistance;
-
-    std::vector<double> m_distances;
-
-    Kinematic m_gearMovement;
-
+    std::map< hpreal,DiscGearGrindResult> m_precalcResults;
+    /**
+     * @brief calculateGrindingDepth calculated the distances from each vertex of the gear ( m_gearRays ) to the disc and stores them in m_distances.
+        For the calculation the rays of the gear with the vertexes as origin and normals as direction are intersected with the disc mesh.
+     * @param time The position in time to which the distances are suppose to be calculated.
+     */
     void calculateGrindingDepth(double time);
+    /**
+     * @brief calculateSimulationResult uses calculateGrindingDepth to calculate the colors of the disc and returns all the information as DiscGearGrindSimulationResult.
+     * @param time The point in time to which the simulation result is calculated.
+     * @return Returns all the information representing the simulation to the given time.
+     */
     DiscGearGrindResult calculateSimulationResult(double time);
 };
 

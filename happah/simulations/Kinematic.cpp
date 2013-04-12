439c36e55aebfa9d57d9cc963417f727ece1d401
Julian Labeit <julian@julian-D2778-A1.(none)>
2013-04-12 13:10:21 +0200

    Continued with documentation

--- happah/simulations/Kinematic.cpp
+++ happah/simulations/Kinematic.cpp
@@ -21,21 +21,21 @@ Kinematic Kinematic::getLinearKinematic(glm::vec3 start, glm::vec3 end, hpreal r
 glm::mat4 Kinematic::getMatrix(double t){
     glm::mat4 result(1);
     // Rotation
-    result = glm::rotate(result, (float)m_rotateX.getY(t), glm::vec3(1,0,0));
-    result = glm::rotate(result, (float)m_rotateY.getY(t), glm::vec3(0,1,0));
-    result = glm::rotate(result, (float)m_rotateZ.getY(t), glm::vec3(0,0,1));
+    result = glm::rotate(result, (float)m_rotateX.evaluateAt(t), glm::vec3(1,0,0));
+    result = glm::rotate(result, (float)m_rotateY.evaluateAt(t), glm::vec3(0,1,0));
+    result = glm::rotate(result, (float)m_rotateZ.evaluateAt(t), glm::vec3(0,0,1));
     // Translation
     result = glm::translate(result,
-                   glm::vec3(m_translateX.getY(t), m_translateY.getY(t), m_translateZ.getY(t)) );
+                   glm::vec3(m_translateX.evaluateAt(t), m_translateY.evaluateAt(t), m_translateZ.evaluateAt(t)) );
     return result;
 }
 
 RigidAffineTransformation Kinematic::getRigidAffineTransformation(double t){
 	glm::mat4 matrix(1);
     // Rotation
-	matrix = glm::rotate(matrix, (float)m_rotateX.getY(t), glm::vec3(1,0,0));
-	matrix = glm::rotate(matrix, (float)m_rotateY.getY(t), glm::vec3(0,1,0));
-	matrix = glm::rotate(matrix, (float)m_rotateZ.getY(t), glm::vec3(0,0,1));
+	matrix = glm::rotate(matrix, (float)m_rotateX.evaluateAt(t), glm::vec3(1,0,0));
+	matrix = glm::rotate(matrix, (float)m_rotateY.evaluateAt(t), glm::vec3(0,1,0));
+	matrix = glm::rotate(matrix, (float)m_rotateZ.evaluateAt(t), glm::vec3(0,0,1));
     // Translation
-    return RigidAffineTransformation( glm::mat3(matrix), hpvec3(m_translateX.getY(t), m_translateY.getY(t), m_translateZ.getY(t)) );
+    return RigidAffineTransformation( glm::mat3(matrix), hpvec3(m_translateX.evaluateAt(t), m_translateY.evaluateAt(t), m_translateZ.evaluateAt(t)) );
 }
--- happah/transformations/RigidAffineTransformation.h
+++ happah/transformations/RigidAffineTransformation.h
@@ -8,22 +8,64 @@
 using namespace std;
 
 class RigidAffineTransformation;
-
 typedef shared_ptr<RigidAffineTransformation> RigidAffineTransformation_ptr;
 
+/**
+ * @brief The RigidAffineTransformation class represents a riggid affine transformation in 3d space
+ *          represented by a rotation matrix and a translation vector.
+ */
 class RigidAffineTransformation {
 
 public:
+    /**
+     * @brief RigidAffineTransformation constructor setting rotation matrix to 1 and translation vector to 0.
+     */
 	RigidAffineTransformation();
+    /**
+     * @brief RigidAffineTransformation contructor setting rotation matrix to matrix and translation vector to translation.
+     * @param matrix rotation matrix.
+     * @param translation translation vector.
+     */
 	RigidAffineTransformation(const hpmat3x3& matrix, const hpvec3& translation);
+    /**
+     * Desctructor doing nothing.
+     */
 	~RigidAffineTransformation();
-
-	void compose(RigidAffineTransformation& rigidAffineTransformation, RigidAffineTransformation& out);
+    /**
+     * @brief compose Composing this transformation with rigidAffineTransformation and storing the result in out.
+     * @param rigidAffineTransformation The transformation with which this transformation is composed.
+     * @param out The transformation in which the result is stored.
+     */
+    void compose(RigidAffineTransformation& rigidAffineTransformation, RigidAffineTransformation& out);
+    /**
+     * @brief getMatrix gets the rotation matrix.
+     * @return the rotation matrix.
+     */
 	hpmat3x3 getMatrix() const;
+    /**
+     * @brief getTranslation gets the translation vector.
+     * @return the translation vector.
+     */
 	hpvec3 getTranslation() const;
+    /**
+     * @brief inverse gets the inverse transformation to this transformation.
+     * @return the inverse transformation.
+     */
 	RigidAffineTransformation inverse()const;
+    /**
+     * @brief setMatrix sets the rotation matrix.
+     * @param matrix the new rotation matrix.
+     */
 	void setMatrix(const hpmat3x3& matrix);
+    /**
+     * @brief setTranslation sets the translation vector.
+     * @param translation the new translation vector.
+     */
 	void setTranslation(const hpvec3& translation);
+    /**
+     * @brief toMatrix4x4 converts the rigid affine transformation to a 4x4 transformation matrix.
+     * @return the transformation matrix.
+     */
 	hpmat4x4 toMatrix4x4() const;
 
 protected:

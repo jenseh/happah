55a8a5201e8bebf91d5feb123bc44fdf55fecc9e
jlabeit <jlabeit@web.de>
2013-04-26 13:10:56 +0200

    Forgot to remove test.

--- happah/main.cpp
+++ happah/main.cpp
@@ -1,7 +1,6 @@
 #include "happah/Happah.h"
 #include "happah-test/benchmarks/KDTreeBenchmark.h"
 int main(int argc, char** argv) {
-	//Happah happah(argc, argv);
-	//return happah.exec();
-	KDTreeBenchmark a;
+	Happah happah(argc, argv);
+	return happah.exec();
 }

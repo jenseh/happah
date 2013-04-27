b4f60e7ca840cf76d8d93cc955bacf7c00a5a461
hcaesar <holger@it-caesar.de>
2013-04-27 19:15:27 +0200

    Removed tests

--- happah/gui/forms/WormForm.cpp
+++ happah/gui/forms/WormForm.cpp
@@ -4,8 +4,6 @@
 #include "happah/geometries/gears/Worm.h"
 #include "happah/gui/forms/WormForm.h"
 
-#include "happah-test/tests/CircleTriangleIntersectionTest.h" //TODO:remove
-
 WormForm::WormForm(GUIManager& guiManager, QWidget* parent)
 	: Form(parent),
 		m_guiManager(guiManager),
@@ -35,8 +33,6 @@ WormForm::WormForm(GUIManager& guiManager, QWidget* parent)
 
 	m_worm = Worm_ptr(new Worm());
 	updateRanges();
-
-	CircleTriangleIntersectionTest(); //TODO:remove
 }
 
 WormForm::~WormForm() {

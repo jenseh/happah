ac5415b952b03f46c1ae73c80a59e08251200536
PeterMerkert <peter.merkert@student.kit.edu>
2013-05-19 14:02:25 +0200

    implementing mergesort for the first entry in the kd-tree. so the triangles are once sorted after their x values

--- happah/kdtree/FastKdTree.cpp
+++ happah/kdtree/FastKdTree.cpp
@@ -1,8 +1,13 @@
 #include "FastKdTree.h"
 
-FastKdTree::FastKdTree(TriangleIterator* begin, TriangleIterator* end) {
+FastKdTree::FastKdTree(TriangleIterator* begin, TriangleIterator* end, int depth) {
     leftChild = NULL;
     rightChild = NULL;
+
+    if (depth == 0) {
+        sort(begin, end);
+    }
+
 }
 
 FastKdTree::~FastKdTree() {
@@ -10,3 +15,48 @@ FastKdTree::~FastKdTree() {
     delete rightChild;
 }
 
+void FastKdTree::sort(TriangleIterator* begin, TriangleIterator* end) {
+    if (begin != end) {
+        //get the length of the list
+        int n = 0;
+        while (begin->operator [](n) != end->operator [](0)) {
+            n++;
+        }
+
+        // if list has only one piece, no need to mergesort again
+        if (n > 0) {
+            sort(begin, begin + n * sizeof(TriangleVerticeRefs*) /2);
+            sort(begin + (n+1) * sizeof(TriangleVerticeRefs*) /2, end);
+            merge(begin,
+                  begin + n * sizeof(TriangleVerticeRefs*) /2,
+                  begin + (n+1) * sizeof(TriangleVerticeRefs*) /2,
+                  end);
+        }
+    }
+}
+
+void FastKdTree::merge(TriangleIterator* first_begin, TriangleIterator* first_end,
+                  TriangleIterator* second_begin, TriangleIterator* second_end) {
+    int indexFront = 0, indexBack = 0;
+    while (first_begin->operator [](indexFront) != first_end->operator [](0)
+           && second_begin->operator [](indexBack) != second_end->operator [](0)) {
+
+        // second list item is lower x
+        if (second_begin->operator [](indexBack)->vertices[0]->x
+            < first_begin->operator [](indexFront)->vertices[0]->x) {
+            switchPointers(second_begin->operator [](indexBack), first_begin->operator [](indexFront));
+            indexFront++;
+        }
+
+        /***********************************************************************************/
+        //TODO: OTHER CASES FOR ALGORITHM
+        /***********************************************************************************/
+
+    }
+}
+
+void FastKdTree::switchPointers(TriangleVerticeRefs* first, TriangleVerticeRefs* second) {
+    TriangleVerticeRefs* tmp = first;
+    first = second;
+    second = tmp;
+}
--- happah/kdtree/FastKdTree.h
+++ happah/kdtree/FastKdTree.h
@@ -21,13 +21,39 @@ private:
      */
     FastKdTree* rightChild;
 
+    /**
+     * @brief sort mergesort the triangles (see more http://de.wikipedia.org/wiki/Mergesort)
+     * @param begin the iterator to iterate over the triangles
+     * @param end the end element to have a stop element for the begin iterator
+     */
+    void sort(TriangleIterator* begin, TriangleIterator* end);
+
+    /**
+     * @brief merge merge the two list into each other
+     * @param frist_begin the frist list start
+     * @param frist_end the first list end
+     * @param second_begin the second list start
+     * @param second_end the second list end
+     */
+    void merge(TriangleIterator* first_begin, TriangleIterator* first_end,
+          TriangleIterator* second_begin, TriangleIterator* second_end);
+
+    /**
+     * @brief switchPointers switch the both pointers via a so called 'Ringtausch'
+     * TODO: Xor binary switch without a temporary variable possible
+     * @param first the first pointer
+     * @param second the second pointer
+     */
+    void switchPointers(TriangleVerticeRefs* first, TriangleVerticeRefs* second);
+
 public:
     /**
      * @brief The constructor builds up the whole fast kd-tree.
      * @param begin the start of the iterator to the triangles
      * @param end the end index to the triangle iterator
+     * @param depth saves the depth for each step
      */
-    FastKdTree(TriangleIterator* begin, TriangleIterator* end);
+    FastKdTree(TriangleIterator* begin, TriangleIterator* end, int depth = 0);
 
     /**
       * @brief destroys the pointer to the child.

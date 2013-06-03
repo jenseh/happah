a93913ac66d051bfdda39919f8188b4cc9e77023
Maximilian Baritz <s_baritz@i08pc04.atis-stud.uni-karlsruhe.de>
2013-06-03 12:57:24 +0200

    Added data structures for Seidel triangulation

--- happah/triangulators/PlanarGraphTriangulatorSeidel.h
+++ happah/triangulators/PlanarGraphTriangulatorSeidel.h
@@ -21,4 +21,67 @@ public:
 		//TriangleMesh2D* toTriangleMesh();
 	};
 
+	class Trapezoid {
+		
+		public:
+			void splitHorizontal(hpvec2& point);
+			void splitVertical(SegmentEndpoints2D& segment);
+
+		private:
+			Sink* sink;
+
+			SegmentEndpoints* leftSegment = NULL;
+			SegmentEndpoints* rightSegment = NULL;
+
+			Trapezoid* upperLeft = NULL;
+			Trapezoid* upperRight = NULL;
+			Trapezoid* lowerLeft = NULL;
+			Trapezoid* lowerRight = NULL;
+	}
+
+	class Node {
+		
+		public:
+			virtual Trapezoid* getTrapezoid(const hpvec2& point) = 0;
+
+			Node* getChild1() {return child1;};
+			Node* getChild2() {return child2;};
+			Node* getParent() {return parent;};
+
+		private:
+			Node* child1 = NULL;
+			Node* child2 = NULL;
+			Node* parent = NULL;
+	}
+
+	class XNode : public Node {
+		
+		public:
+			Trapezoid* getTrapezoid(const hpvec2& point);
+
+		private:
+			SegmentEndpoints2D* key;
+	}
+
+	class YNode : public Node {
+		
+		public:
+			Trapezoid* getTrapezoid(const hpvec2& point);
+
+		private:
+			hpdouble key;
+	}
+
+	class Sink : public Node {
+
+		public:
+			Trapezoid* getTrapezoid(const hpvec2& point);
+			YNode* splitHorizontal(hpdouble yValue);
+			XNode* splitVertical(SegmentEndpoints2D* segment);
+
+		private:
+			Trapezoid* trapezoid;
+	}
 };
+
+
